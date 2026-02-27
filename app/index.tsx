72a043f016499fef47bacffd8737ed060ceb0032
JuanMCelis <163361602+JuanMCelis@users.noreply.github.com>
2026-02-27 18:45:04 -0500

    Add files via upload

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
+import { useRouter} from "expo-router";
 import React, { useState } from "react";
-import { TextInput, View, Text, Button } from "react-native";
+import { Button, Text, TextInput, View,Alert } from "react-native";
 
 
 const IndexScreen =()=> {
     
     const[textValue,setTextValue] = useState("");
     const[message, setMessage] = useState("");
-
+    const router = useRouter();
 
     return <View>
     
@@ -22,11 +23,23 @@ const IndexScreen =()=> {
         title="Ingresar"
         onPress={()=>{
         if (textValue === "hola123") {
-            
+        router.push("/Notas");
           setMessage("Contraseña correcta");
         }else {
         setMessage("Contraseña incorrecta");
-            }
+            Alert.alert(
+              'Algo ha salido mal', 
+              'Wrong Password', 
+              [
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel',
+                },
+                { text: 'OK', onPress: () => console.log('OK Pressed') },
+              ]
+            );
+        }
         }}
     />
     <Text>{message}</Text>
